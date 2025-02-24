44fd615bb7d963f2206ba3eb3249da2cbbf2f6c3
Leo <ldnava10@yahoo.com>
2025-02-23 18:49:38 -0800

    cpsc 449 proj hope this works correclty

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import AboutPage from "mechakeys/public/Pages/AboutPage.js";
-import LoginPage from "mechakeys/public/Pages/KeyboardsPage.js";
-import KeyboardPage from "mechakeys/public/Pages/LoginPage.js";
-const App = () => {
+import AboutPage from "./Pages/AboutPage";
+import KeyboardPage from "./Pages/KeyboardsPage";
+import LoginPage from "./Pages/LoginPage";
+function App() {
   return (
     <Router>
-      {/* Navigation Bar */}
-      <nav className="p-4 bg-gray-200">
-        <ul className="flex space-x-4">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/keyboard">Keyboards</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link> | 
+        <Link to="/keyboard">Keyboards</Link> | 
+        <Link to="/login">Login</Link>
       </nav>
 
-      {/* Routes */}
       <Routes>
-        <Route path="/" element={<h1 className="text-center mt-10">this is from the app.js</h1>} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/keyboard" element={<KeyboardPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
---
+++ src/Pages/KeyboardsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function KeyboardPage() {
+  return (
+    <div>
+      <h1>Keyboards</h1>
+      <p>Browse our selection of high-quality mechanical keyboards.</p>
+    </div>
+  );
+}
+
+export default KeyboardPage;
