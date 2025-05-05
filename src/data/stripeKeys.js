93d43303febf54ff734c66de9123a60eeebbb8ff
g0chu <51940197+g0chu@users.noreply.github.com>
2025-05-04 20:06:55 -0700

    implemented Stripe for handling checkout
    Can do a full mock checkout from picking keyboard to order success page (use fake information for Stripe checkout).
    
    MUST let order success page fully load after completing checkout for backend to successfully update with new order.

---
+++ src/data/stripeKeys.js
@@ -0,0 +1,7 @@
+export const STRIPE_KEYS = [
+    {
+      PUBLISHABLE_KEY: 'pk_test_51RLCVyGbQm6wkdOsof3OXIBCUqnccW1EwLS3ZjAGWV6XBnsKTwEo5csYGmZWDdeU3wsuI8QrUAuGoVU1gQqctmBE00MHcaT3mm',
+      SECRET_KEY: 'sk_test_51RLCVyGbQm6wkdOsI6GjcX5UIowVuK0cGrq8XsGLpdWFnwb3SqUaDvRkJH0WlUT7uZclnKMRL6TG7kGxaU0RubZi00hxO9wLWa'  
+    },
+  ];
+  
